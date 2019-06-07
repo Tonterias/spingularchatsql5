8323bb029070dacf550d31beefb1a60335dea3f1
Tonterias <ecorreos@htomail.com>
2019-06-07 18:03:55 +0200

    MultiRooms showing messages and ChatUser saved ChatMessages in database

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,9 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
-import { JhiEventManager } from 'ng-jhipster';
+
 import { ChatService } from '../shared';
 
 import { LoginModalService, AccountService, Account } from 'app/core';
+
+import { HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { filter, map } from 'rxjs/operators';
+import { JhiEventManager, JhiParseLinks, JhiAlertService } from 'ng-jhipster';
+
+import { IChatRoom } from 'app/shared/model/chat-room.model';
+
+import { ITEMS_PER_PAGE } from 'app/shared';
+import { ChatRoomService } from '../entities/chat-room/chat-room.service';
+
+import { IChatMessage } from 'app/shared/model/chat-message.model';
+import { ChatMessageService } from '../entities/chat-message/chat-message.service';
+
+import { IChatUser } from 'app/shared/model/chat-user.model';
+import { ChatUserService } from '../entities/chat-user/chat-user.service';
 
 @Component({
   selector: 'jhi-home',
@@ -16,11 +33,39 @@ export class HomeComponent implements OnInit {
   messages: Array<Object> = [];
   message = '';
 
+  chatMessage: IChatMessage;
+  chatMessages: IChatMessage[];
+  chatUser: IChatUser;
+  chatUsers: IChatUser[];
+  chatRooms: IChatRoom[];
+
+  currentAccount: any;
+  error: any;
+  success: any;
+  eventSubscriber: Subscription;
+  routeData: any;
+  links: any;
+  totalItems: any;
+  itemsPerPage: any;
+  page: any;
+  predicate: any;
+  previousPage: any;
+  reverse: any;
+
+  currentChatRoomId: number;
+
   constructor(
     private accountService: AccountService,
     private loginModalService: LoginModalService,
     private eventManager: JhiEventManager,
-    private chatService: ChatService
+    private chatService: ChatService,
+    protected chatRoomService: ChatRoomService,
+    protected chatMessageService: ChatMessageService,
+    protected chatUserService: ChatUserService,
+    protected parseLinks: JhiParseLinks,
+    protected jhiAlertService: JhiAlertService,
+    protected activatedRoute: ActivatedRoute,
+    protected router: Router
   ) {}
 
   ngOnInit() {
@@ -32,7 +77,24 @@ export class HomeComponent implements OnInit {
 
     this.accountService.identity().then(account => {
       this.account = account;
+      const query = {};
+      query['id.equals'] = this.account.id;
+      //      console.log('CONSOLOG: M:ngOnInit & O: query : ', query);
+      this.chatUserService.query(query).subscribe(
+        (res: HttpResponse<IChatUser[]>) => {
+          this.chatUser = res.body[0];
+          //          console.log('CONSOLOG: M:ngOnInit & O: this.chatUser : ', this.chatUser);
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
     });
+
+    this.loadAll();
+    this.accountService.identity().then(account => {
+      this.currentAccount = account;
+    });
+    this.registerChangeInChatRooms();
+
     this.registerAuthenticationSuccess();
     this.registerLogoutSuccess();
   }
@@ -66,7 +128,62 @@ export class HomeComponent implements OnInit {
     if (message.length === 0) {
       return;
     }
-    this.chatService.sendMessage(message);
+    this.chatMessage = new Object();
+    this.chatMessage.chatUserId = this.account.id;
+    this.chatMessage.chatRoomId = this.currentChatRoomId;
+    this.chatMessage.message = message;
+    console.log('CONSOLOG: M:sendMessage & O: this.chatMessage: ', this.chatMessage);
+    this.chatService.sendMessage(this.chatMessage);
     this.message = '';
+  }
+
+  loadAll() {
+    this.chatRoomService
+      .query({})
+      .subscribe(
+        (res: HttpResponse<IChatRoom[]>) => this.paginateChatRooms(res.body, res.headers),
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+  }
+
+  fetchChatRoom(chatRoomId) {
+    //    console.log('CONSOLOG: M:fetchChatRoom & O: chatRoomId : ', chatRoomId);
+    this.currentChatRoomId = chatRoomId;
+    if (chatRoomId !== undefined) {
+      const query = {};
+      query['chatRoomId.equals'] = this.currentChatRoomId;
+      //      query['queryParams'] = 1;
+      console.log('CONSOLOG: M:fetchChatRoom & O: query : ', query);
+      this.chatMessageService.query(query).subscribe(
+        (res: HttpResponse<IChatMessage[]>) => {
+          this.messages = res.body;
+          console.log('CONSOLOG: M:fetchChatRoom & O: this.messages : ', this.messages);
+        },
+        (res: HttpErrorResponse) => this.onError(res.message)
+      );
+    }
+  }
+
+  registerChangeInChatRooms() {
+    this.eventSubscriber = this.eventManager.subscribe('chatRoomListModification', response => this.loadAll());
+  }
+
+  sort() {
+    const result = [this.predicate + ',' + (this.reverse ? 'asc' : 'desc')];
+    if (this.predicate !== 'id') {
+      result.push('id');
+    }
+    return result;
+  }
+
+  protected paginateChatRooms(data: IChatRoom[], headers: HttpHeaders) {
+    this.links = this.parseLinks.parse(headers.get('link'));
+    this.totalItems = parseInt(headers.get('X-Total-Count'), 10);
+    this.chatRooms = data;
+    console.log('CONSOLOG: M:paginateChatRooms & O: this.chatRooms : ', this.chatRooms);
+  }
+
+  protected onError(errorMessage: string) {
+    this.jhiAlertService.error(errorMessage, null, null);
   }
 }
